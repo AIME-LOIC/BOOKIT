d66b52826ea25aaa3a3e3b0c1c7e1b01c11e3881
AIME-LOIC <kingaime132@gmail.com>
2025-05-06 13:58:08 +0200

    saved

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask, jsonify, render_template
 import json
 
 app = Flask(__name__)
+
 @app.route('/')
 def index():
-     return render_template('index.html')
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(port=2004)
-    app.run(debug=true)
+@app.route('/get_buses')
+def get_buses():
+    with open('base.json') as f:
+        data = json.load(f)
+    return jsonify(data)
 
+if __name__ == '__main__':
+    app.run(port=8000)
+    app.run(debug=True)
